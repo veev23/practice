9389c2ceea99ef8e2608f1e55e5522a69a248386
veev23 <veev23@gmail.com>
2018-04-09 22:50:46 +0900

    order 구현, insert 미구현(오류)

--- TreeBinary.cpp
+++ TreeBinary.cpp
@@ -1,55 +1,78 @@
 #include <iostream>
 using namespace std;
 template <typename T>
-class Node{
+class Node {
 public:
 	Node() {
 		left = 0;
 		right = 0;
 	}
-	Node(T _data)data(_data) {};
-private:
+	Node(T _data) :data(_data) {};
+	template <typename T> friend class TreeBinary;
 	T data;
 	Node* left;
 	Node* right;
 };
 template <typename T>
 class TreeBinary {
+protected:
+	Node<T>* root;
+	int m_numNode;
 public:
 	TreeBinary()
 		:m_numNode(0)
 	{
-		root = 0;
-		root->left = 0;
-		root->right = 0;
+		cout << "BT constructor" << endl;
+	}
+	TreeBinary(T _data)
+		:m_numNode(1)
+	{
+		root = new Node<T>(_data);
+		cout << "BT constructor, data : " << root->data << endl;
 	}
 	~TreeBinary() {
 		RemoveAll(root);
+		cout << "BT destructor" << endl;
 	}
-	RemoveAll(Node* delNode) {
+	void RemoveAll(Node<T>* delNode) {
 		if (delNode) {
 			RemoveAll(delNode->left);
 			RemoveAll(delNode->right);
+			cout << delNode->data << " delete\n";
 			delete delNode;
 		}
 	}
-	Preorder(Node* travel) {
-		cout << travel->data << " ";
-		Preorder(travel->left);
-		Preorder(travel->right);
+	void PreorderPrint() {
+		Preorder(root);
 	}
-	Inorder(Node* travel) {
-		Preorder(travel->left);
-		cout << travel->data << " ";
-		Preorder(travel->right);
+	void Preorder(Node<T>* travel) {
+		if (travel) {
+			cout << "visit " << travel->data << endl;
+			Preorder(travel->left);
+			Preorder(travel->right);
+		}
 	}
-	Postorder(Node* travel) {
-		Preorder(travel->left);
-		Preorder(travel->right);
-		cout << travel->data << " ";
+	void InorderPrint() {
+		Inorder(root);
 	}
-
-private:
-	Node<T>* root;
-	int m_numNode;
+	void Inorder(Node<T>* travel) {
+		if (travel) {
+			Inorder(travel->left);
+			cout << "visit " << travel->data << endl;
+			Inorder(travel->right);
+		}
+	}
+	void PostorderPrint() {
+		Postorder(root);
+	}
+	void Postorder(Node<T>* travel) {
+		if (travel) {
+			Postorder(travel->left);
+			Postorder(travel->right);
+			cout << "visit " << travel->data << endl;
+		}
+	}
+	int GetNodeNumber() {
+		return m_numNode;
+	}
 };
--- TreeBinarySearch.cpp
+++ TreeBinarySearch.cpp
@@ -1,4 +1,50 @@
+#include "TreeBinary.cpp"
 template <typename T>
-class BinaryTree :public Tree {
-
+class TreeBinarySearch
+	:public TreeBinary<T>
+{
+public:
+	TreeBinarySearch() {
+		cout << "BST constructor" << endl;
+	}
+	TreeBinarySearch(T _data)
+		:TreeBinary<T>(_data)
+	{
+		cout << "BST constructor, data : " << _data << endl;
+	}
+	~TreeBinarySearch() {
+		cout << "BST destructor" << endl;
+	}
+	void insert(T _data) {
+		Node<T>*tmp= root;
+		if (root) {
+			root = new Node<T>(_data);
+			m_numNode++;
+			return;
+		}
+		while (true) {
+			if (tmp->data > _data) {
+				if (tmp->right) tmp = tmp->right;
+				else {
+					tmp->right->data = _data;		
+					m_numNode++;
+					return;
+				}
+			}
+			else {
+				if (tmp->left) tmp = tmp->left;
+				else { 
+					tmp->left->data = _data;
+					m_numNode++;
+					return;
+				}
+			}
+		}
+	}
 };
+int main() {
+	TreeBinarySearch<int> B(1);
+	B.PreorderPrint();
+//	B.insert(2);
+	return 0;
+}
